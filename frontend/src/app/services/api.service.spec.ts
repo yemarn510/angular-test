634258893ea022c8610cca50c40260bbd55a87b3
yemarn <yemarn.510@gmail.com>
2024-08-08 23:44:44 +0700

    added more test for api service

--- frontend/src/app/services/api.service.spec.ts
+++ frontend/src/app/services/api.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
-import { HttpClient, HttpErrorResponse, HttpHeaders, provideHttpClient } from '@angular/common/http';
+import { HttpErrorResponse, provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { of, throwError } from 'rxjs';
 
@@ -9,7 +9,7 @@ describe('ApiService', () => {
   let apiServiceSpy: jasmine.SpyObj<ApiService>;
 
   beforeEach(() => {
-    apiServiceSpy = jasmine.createSpyObj('ApiService', ['get', 'post', 'put', 'delete', 'checkRequest', 'getHeaders']);
+    apiServiceSpy = jasmine.createSpyObj('ApiService', ['get', 'post', 'put', 'patch', 'delete', 'checkRequest', 'getHeaders']);
 
     TestBed.configureTestingModule({
       providers: [ 
@@ -46,7 +46,7 @@ describe('ApiService', () => {
     });
   });
 
-  it('should return response on success', () => {
+  it('should return response on success for get method', () => {
     const response = { data: 'data' };
     apiServiceSpy.get.and.returnValue(of(response));
     apiServiceSpy.get('url').subscribe({
@@ -57,9 +57,104 @@ describe('ApiService', () => {
     });
   });
 
-  it('should call checkRequest method in error response', () => {
+  it('should return response on success for get method with params', () => {
+    const response = { data: 'data' };
+    apiServiceSpy.get.and.returnValue(of(response));
+    apiServiceSpy.get('url').subscribe({
+      next: (res) => {
+        expect(res).toEqual(response);
+      },
+      error: () => {}
+    });
+  });
+
+  it('should return response on success post method', () => {
+    const response = { data: 'data' };
+    apiServiceSpy.post.and.returnValue(of(response));
+    apiServiceSpy.post('url', {}).subscribe({
+      next: (res) => {
+        expect(res).toEqual(response);
+      },
+      error: () => {}
+    });
+  });
+
+  it('should return response on success patch method', () => {
+    const response = { data: 'data' };
+    apiServiceSpy.patch.and.returnValue(of(response));
+    apiServiceSpy.patch('url', {}).subscribe({
+      next: (res) => {
+        expect(res).toEqual(response);
+      },
+      error: () => {}
+    });
+  });
+
+  it('should return response on success put method', () => {
+    const response = { data: 'data' };
+    apiServiceSpy.put.and.returnValue(of(response));
+    apiServiceSpy.put('url', {}).subscribe({
+      next: (res) => {
+        expect(res).toEqual(response);
+      },
+      error: () => {}
+    });
+  });
+
+  it('should return response on success delete method', () => {
+    const response = { data: 'data' };
+    apiServiceSpy.delete.and.returnValue(of(response));
+    apiServiceSpy.delete('url').subscribe({
+      next: (res) => {
+        expect(res).toEqual(response);
+      },
+      error: () => {}
+    });
+  });
+
+  it('should call checkRequest method in error response for get method', () => {
     apiServiceSpy.get.and.returnValue(throwError(() => apiServiceSpy.checkRequest(new HttpErrorResponse({ status: 404 }))));
     apiServiceSpy.get('url').subscribe({
+      next: () => {},
+      error: () => {
+        expect(apiServiceSpy.checkRequest).toHaveBeenCalled();
+      }
+    });
+  });
+
+  it('should call checkRequest method in error response for post method', () => {
+    apiServiceSpy.post.and.returnValue(throwError(() => apiServiceSpy.checkRequest(new HttpErrorResponse({ status: 404 }))));
+    apiServiceSpy.post('url', {}).subscribe({
+      next: () => {},
+      error: () => {
+        expect(apiServiceSpy.checkRequest).toHaveBeenCalled();
+      }
+    });
+  });
+
+  it('should call checkRequest method in error response for patch method', () => {
+    apiServiceSpy.patch.and.returnValue(throwError(() => apiServiceSpy.checkRequest(new HttpErrorResponse({ status: 404 }))));
+    apiServiceSpy.patch('url', {}).subscribe({
+      next: () => {},
+      error: () => {
+        expect(apiServiceSpy.checkRequest).toHaveBeenCalled();
+      }
+    });
+  });
+
+  it('should call checkRequest method in error response for put method', () => {
+    apiServiceSpy.put.and.returnValue(throwError(() => apiServiceSpy.checkRequest(new HttpErrorResponse({ status: 404 }))));
+    apiServiceSpy.put('url', {}).subscribe({
+      next: () => {},
+      error: () => {
+        expect(apiServiceSpy.checkRequest).toHaveBeenCalled();
+      }
+    });
+  });
+
+  it('should call checkRequest method in error response for delete method', () => {
+    apiServiceSpy.delete.and.returnValue(throwError(() => apiServiceSpy.checkRequest(new HttpErrorResponse({ status: 404 }))));
+    apiServiceSpy.delete('url').subscribe({
       next: () => {},
       error: () => {
         expect(apiServiceSpy.checkRequest).toHaveBeenCalled();
