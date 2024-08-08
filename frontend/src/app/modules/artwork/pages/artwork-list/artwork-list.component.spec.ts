5915d752b6182f7ecebafd90176334666fab0efd
yemarn <yemarn.510@gmail.com>
2024-08-08 22:16:06 +0700

    added proper list for artwork-list component test

--- frontend/src/app/modules/artwork/pages/artwork-list/artwork-list.component.spec.ts
+++ frontend/src/app/modules/artwork/pages/artwork-list/artwork-list.component.spec.ts
@@ -2,21 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArtworkListComponent } from './artwork-list.component';
 import { ArtWorkModule } from '../../artwork.module';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { Artwork, ArtworkStyleDropdown } from '../../../../models/artwork.model';
-import { ApiService } from '../../../../services/api.service';
-import { ArtworkService } from '../../artwork.service';
-import { PaginatedResponse } from '../../../../models/common.model';
 import { DUMMY_RESPONSE } from '../../../../constants/mocked-artworks.constant';
 
 
 describe('ArtworkListComponent', () => {
   let component: ArtworkListComponent;
   let fixture: ComponentFixture<ArtworkListComponent>;
-  let artworkService: ArtworkService;
-  let apiService: ApiService;
-  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -28,8 +22,6 @@ describe('ArtworkListComponent', () => {
       declarations: [ArtworkListComponent],
     })
     .compileComponents();
-
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
     fixture = TestBed.createComponent(ArtworkListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -119,4 +111,97 @@ describe('ArtworkListComponent', () => {
     component.goToPage(2);
     expect(component.getArtworks).toHaveBeenCalled();
   });
+
+  it('should call getArtworks on component rendered', () => {
+    spyOn(component, 'getArtworks');
+    component.getArtworks();
+    expect(component.getArtworks).toHaveBeenCalled();
+  });
+
+  // ---------------- filterAndSortArtworks function ----------------
+  it('should call filterArtworks on filterAndSortArtworks function call', () => {
+    spyOn(component, 'filterArtworks');
+    component.filterAndSortArtworks();
+    expect(component.filterArtworks).toHaveBeenCalled();
+  });
+
+  it('should call sortArtworks on filterAndSortArtworks function call', () => {
+    spyOn(component, 'sortArtworks');
+    component.filterAndSortArtworks();
+    expect(component.sortArtworks).toHaveBeenCalled();
+  });
+
+  // ---------------- sortArtworks function ----------------
+  it('should return all data in the list for blank sort', () => {
+    const mockedArtworks: Artwork[] = DUMMY_RESPONSE.data;
+    component.sortBy = '';
+    expect(component.sortArtworks(mockedArtworks)).toEqual(mockedArtworks);
+  });
+
+  it('should return sorted data in the list for selected sort by title', () => {
+    const mockedArtworks: Artwork[] = DUMMY_RESPONSE.data;
+    component.sortBy = 'title';
+    const expectedArtworks: Artwork[] = DUMMY_RESPONSE.data.sort((a, b) => a.title.localeCompare(b.title));
+    expect(component.sortArtworks(mockedArtworks)).toEqual(expectedArtworks);
+  });
+
+  it('should return sorted data in the list for selected sort by artist_title', () => {
+    const mockedArtworks: Artwork[] = DUMMY_RESPONSE.data;
+    component.sortBy = 'artist_title';
+    const expectedArtworks: Artwork[] = DUMMY_RESPONSE.data.sort((a, b) => a.artist_title.localeCompare(b.artist_title));
+    expect(component.sortArtworks(mockedArtworks)).toEqual(expectedArtworks);
+  });
+
+  it('should return sorted data in the list for selected sort by date_start', () => {
+    const mockedArtworks: Artwork[] = DUMMY_RESPONSE.data;
+    component.sortBy = 'date_start';
+    const expectedArtworks: Artwork[] = DUMMY_RESPONSE.data.sort((a, b) => new Date(a.date_start).getTime() - new Date(b.date_start).getTime());
+    expect(component.sortArtworks(mockedArtworks)).toEqual(expectedArtworks);
+  });
+
+  it('should return empty list for empty list and selected sort', () => {
+    const mockedArtworks: Artwork[] = [];
+    component.sortBy = 'title';
+    expect(component.sortArtworks(mockedArtworks)).toEqual([]);
+  });
+
+  it('should return empty list for empty list and empty selected sort', () => {
+    const mockedArtworks: Artwork[] = [];
+    component.sortBy = '';
+    expect(component.sortArtworks(mockedArtworks)).toEqual([]);
+  });
+
+  it('should return empty list for empty list and invalid selected sort', () => {
+    const mockedArtworks: Artwork[] = [];
+    component.sortBy = 'invalid';
+    expect(component.sortArtworks(mockedArtworks)).toEqual([]);
+  });
+
+  it('should return sorted data in the list for selected date sort', () => {
+    const mockedArtworks: Artwork[] = DUMMY_RESPONSE.data;
+    component.sortBy = 'date_start';
+    const expectedArtworks: Artwork[] = DUMMY_RESPONSE.data.sort((a, b) => new Date(a.date_start).getTime() - new Date(b.date_start).getTime());
+    expect(component.sortArtworks(mockedArtworks)).toEqual(expectedArtworks);
+  });
+
+  it('should return empty list for empty list and selected date sort', () => {
+    const mockedArtworks: Artwork[] = [];
+    component.sortBy = 'date_start';
+    expect(component.sortArtworks(mockedArtworks)).toEqual([]);
+  });
+
+  // ---------------- Global Variables ----------------
+  it('should have the list of sorting options', () => {
+    const expectedOptions = ['title', 'artist_title', 'date_start'];
+    expect(component.sortByList).toEqual(expectedOptions);
+  });
+
+  it('should have the label for all sorting options', () => {
+    const keys = component.sortByList;
+    const expectedOptions = ['Name', 'Artist', 'Date'];
+    for (let index = 0; index < keys.length; index++) {
+      expect(component.sortByValueAndLabelConnection[keys[index]]).toEqual(expectedOptions[index]);
+    }
+  });
+
 });
